10e6281ced765f349e9b60561671a83069614a88
hyjoong <maplesosser@naver.com>
2021-08-02 19:31:45 +0900

    feat: router setting

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <>Hi</>;
-}
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Main from "./pages/main";
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
