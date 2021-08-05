cf63368672588f9c0fb4b1924c0bac998fd3cac5
hyjoong <maplesosser@naver.com>
2021-08-05 18:37:06 +0900

    feat: server setting

---
+++ server/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(morgan("tiny"));
+
+app.use("/auth", authRouter);
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.sendStatus(500);
+});
+app.listen(8080);
