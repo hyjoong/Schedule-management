747659bce54f49c18c441b791616baabc198b696
hyjoong <maplesosser@naver.com>
2021-08-06 15:22:43 +0900

    feat: findUser & createUser api implement

---
+++ server/data/auth.js
@@ -0,0 +1,9 @@
+export async function findByUsername(username) {
+  return users.find((user) => user.username === username);
+}
+
+export async function createUser(user) {
+  const created = { ...user, id: Date.now().toString };
+  user.push(created);
+  return created.id;
+}
