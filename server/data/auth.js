ed6efd95321bebeac7ab2900deec60a63c3cf1dc
hyjoong <maplesosser@naver.com>
2021-08-06 15:24:41 +0900

    feat: make user mock data

--- server/data/auth.js
+++ server/data/auth.js
@@ -1,3 +1,13 @@
+let users = [
+  {
+    id: "1",
+    email: "hyunjoong12@naver.com",
+    password: "$2b$12sdsamdlasdWQmsdlsmaJENOFI0d80137s6@!$s",
+    username: "hyunjoong",
+    nickname: "hyunla",
+  },
+];
+
 export async function findByUsername(username) {
   return users.find((user) => user.username === username);
 }
