b1c08348f3d3bcb4aa086e8efd66ec92157bbcc9
hyjoong <maplesosser@naver.com>
2021-11-13 23:23:53 +0900

    feat: plan implement redux-saga

--- client/src/redux/action.js
+++ client/src/redux/action.js
@@ -1,52 +1,8 @@
-import {
-  LOAD_SCHEDULE,
-  ADD_SCHEDULE,
-  LOGIN,
-  LOGOUT,
-  DELETE_SCHEDULE,
-  LOGIN_SUCCESS,
-  LOGIN_FAILURE,
-  LOGOUT_FAILURE,
-} from "./actionType";
-
-const LoadScheduleAction = (data) => {
-  return {
-    type: LOAD_SCHEDULE,
-    data,
-  };
-};
-
-const AddSchedule = (data) => {
-  return {
-    type: ADD_SCHEDULE,
-    data,
-  };
-};
-
-const DeleteSchedule = (data) => {
-  return {
-    type: DELETE_SCHEDULE,
-    data,
-  };
-};
+import { ADD_PLAN, LOGIN, LOGOUT } from "./actionType";
 
 const LoginAction = (data) => {
   return {
     type: LOGIN,
-    data,
-  };
-};
-
-const LoginSuccessAction = (data) => {
-  return {
-    type: LOGIN_SUCCESS,
-    data,
-  };
-};
-
-const LoginFailAction = (data) => {
-  return {
-    type: LOGIN_FAILURE,
     data,
   };
 };
@@ -57,28 +13,11 @@ const LogoutAction = () => {
   };
 };
 
-const LogoutSuccessAction = (data) => {
+const AddPlan = (data) => {
   return {
-    type: LOGIN_SUCCESS,
+    type: ADD_PLAN,
     data,
   };
 };
 
-const LogoutFailAction = (data) => {
-  return {
-    type: LOGOUT_FAILURE,
-    data,
-  };
-};
-
-export {
-  LoadScheduleAction,
-  AddSchedule,
-  LoginAction,
-  LogoutAction,
-  DeleteSchedule,
-  LoginSuccessAction,
-  LoginFailAction,
-  LogoutSuccessAction,
-  LogoutFailAction,
-};
+export { LoginAction, LogoutAction, AddPlan };
--- client/src/redux/reducers/schedule.js
+++ client/src/redux/reducers/schedule.js
@@ -1,4 +1,19 @@
-import { LOAD_SCHEDULE, ADD_SCHEDULE, DELETE_SCHEDULE } from "../actionType";
+import produce from "immer";
+
+import {
+  LOAD_PLAN,
+  ADD_PLAN,
+  DELETE_PLAN,
+  LOAD_PLAN_SUCCESS,
+  LOAD_PLAN_FAILURE,
+  ADD_PLAN_SUCCESS,
+  ADD_PLAN_FAILURE,
+  UPDATE_PLAN,
+  UPDATE_PLAN_SUCCESS,
+  UPDATE_PLAN_FAIILURE,
+  DELETE_PLAN_SUCCESS,
+  DELETE_PLAN_FAILURE,
+} from "../actionType";
 
 const initialState = {
   count: 1,
@@ -25,31 +40,93 @@ const initialState = {
       end: "2021-08-20",
     },
   ],
+  addPlanLoading: false,
+  addPlanDone: false,
+  addPlanError: null,
+  updatePlanLoading: false,
+  updatePlanDone: false,
+  updatePlanError: null,
+  donePlanLoading: false,
+  donePlanDone: false,
+  donePlanError: null,
+  deletePlanLoading: false,
+  deletePlanDone: false,
+  deletePlanError: null,
+  loadPlanLoading: false,
+  loadPlanDone: false,
+  loadPlanError: null,
 };
 
 const ScheduleReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case LOAD_SCHEDULE:
-      return {
-        ...state,
-        scheduleData: action.data,
-      };
-    case ADD_SCHEDULE:
-      return {
-        ...state,
-        scheduleData: [...state.scheduleData, action.data],
-      };
-    case DELETE_SCHEDULE:
-      console.log(action.data);
-      return {
-        ...state,
-        scheduleData: state.scheduleData.filter(
-          (data) => data.id !== action.data
-        ),
-      };
-    default:
-      return state;
-  }
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case LOAD_PLAN:
+        draft.loadPlanLoading = true;
+        draft.loadPlanDone = false;
+        draft.loadPlanError = null;
+        break;
+      case LOAD_PLAN_SUCCESS:
+        draft.loadPlanLoading = false;
+        draft.loadPlanDone = true;
+        draft.planData = action.data;
+        break;
+      case LOAD_PLAN_FAILURE:
+        draft.loadPlanLoading = false;
+        draft.loadPlanError = action.error;
+        break;
+      case ADD_PLAN:
+        draft.addPlanLoading = true;
+        draft.addPlanDone = false;
+        draft.addPlanError = null;
+        break;
+      case ADD_PLAN_SUCCESS:
+        draft.addPlanLoading = false;
+        draft.addPlanDone = true;
+        draft.scheduleData.push(action.data);
+        break;
+      case ADD_PLAN_FAILURE:
+        draft.addPlanLoading = false;
+        draft.addPlanError = action.error;
+        break;
+      case UPDATE_PLAN:
+        draft.updatePlanLoading = true;
+        draft.updatePlanDone = false;
+        draft.updatePlanError = null;
+        break;
+      case UPDATE_PLAN_SUCCESS:
+        const plan = draft.scheduleData;
+        plan[
+          draft.scheduleData.findIndex((plan) => plan.id === action.data.id)
+        ] = action.data;
+        draft.scheduleData = plan;
+        draft.updatePlanLoading = true;
+        draft.updatePlanDone = false;
+        draft.updatePlanError = null;
+        break;
+      case UPDATE_PLAN_FAIILURE:
+        draft.updatePlanLoading = false;
+        draft.updatePlanError = action.error;
+        break;
+      case DELETE_PLAN:
+        draft.removePlanLoading = true;
+        draft.removePlanDone = false;
+        draft.removePlanError = null;
+        break;
+      case DELETE_PLAN_SUCCESS:
+        draft.removePlanLoading = false;
+        draft.removePlanDone = true;
+        draft.scheduleData = draft.scheduleData.filter(
+          (f) => f.id === action.data.id
+        );
+        break;
+      case DELETE_PLAN_FAILURE:
+        draft.removePlanLoading = false;
+        draft.removePlanError = action.error;
+        break;
+      default:
+        return state;
+    }
+  });
 };
 
 export default ScheduleReducer;
