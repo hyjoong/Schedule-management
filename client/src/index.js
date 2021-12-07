cd84e3032a65aa81bedea3f35888ef1fe83301ac
hyjoong <maplesosser@naver.com>
2021-12-07 17:45:31 +0900

    feat: redux-persist 적용 [#6]

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
+import { persistStore } from "redux-persist";
 import App from "./App";
 import GlobalStyle from "./styles/GlobalStyle";
 import { theme } from "./styles/theme";
 import ConfigureStore from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 const store = ConfigureStore;
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,7 +20,9 @@ ReactDOM.render(
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <BrowserRouter>
-          <App />
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
         </BrowserRouter>
       </ThemeProvider>
     </Provider>
--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,10 +1,18 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import ScheduleReducer from "./schedule";
 import authReducer from "./auth";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["authReducer"],
+};
 
 const rootReducer = combineReducers({
   authReducer,
   ScheduleReducer,
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
