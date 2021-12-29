ecab33fe8cf6701a25e848ad4fcc210907359d4a
hyjoong <maplesosser@naver.com>
2021-12-29 16:18:41 +0900

    feat: heroku deployment

--- server/database/database.js
+++ server/database/database.js
@@ -1,10 +1,17 @@
 import SQ from "sequelize";
 import { config } from "../config.js";
 
-const { host, user, database, password } = config.db;
+const { host, port, user, database, password } = config.db;
 
 export const sequelize = new SQ.Sequelize(database, user, password, {
   host,
-  dialect: "mysql",
+  port,
+  dialect: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
